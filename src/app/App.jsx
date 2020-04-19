33273202eb21fbb79dde4be027886cd2b8de7a1d
Venusta <Venusta>
2020-04-19 12:43:11 +0100

    Sidebar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Header from "./components/Header/Header";
 import "./App.css";
 import ListFromStore from "./components/ListFromStore/ListFromStore";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => (
-  <div>
+  <div className="app">
+    <Sidebar />
     <Header text="Hello, world." />
     <ListFromStore />
   </div>
---
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable arrow-body-style */
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebarTitle">Xplorable</div>
+    </div>
+  );
+};
+
+export default Sidebar;
